fbffebf6e06909cc66ba16e8e4944b0650d59819
dnascimento <douglassnasciment@gmail.com>
2024-02-06 15:53:04 -0300

    first commit

---
+++ backend/src/main/java/br/reserfy/ReserfyApplication.java
@@ -0,0 +1,13 @@
+package br.reserfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserfyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/reserfy/ReserfyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.reserfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
